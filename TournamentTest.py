0e48d99d79b49977d77c03319039431d72fa0cdb
Voldemert1337 <56188868+Voldemert1337@users.noreply.github.com>
2024-09-07 18:42:27 +0300

    Add files via upload

---
+++ TournamentTest.py
@@ -0,0 +1,48 @@
+from runner import Runner
+from runner import Tournament
+import unittest
+
+
+
+class TournamentTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
+
+    def setUp(self):
+        self.runners = [
+            Runner("Усэйн", 10),
+            Runner("Андрей", 9),
+            Runner("Ник", 3),
+        ]
+
+    @classmethod
+    def tearDownClass(cls):
+        for key, value in cls.all_results.items():
+            print("{}:".format(key))
+            for k, v in value.items():
+                print("{}: {}".format(k, v))
+
+    def test_usain_and_nick(self):
+        turn_1 = Tournament(90, self.runners[0], self.runners[2])
+        result = turn_1.start()
+        self.assertTrue(list(result.values())[-1].name == "Ник")
+        self.all_results["Результат Усэйна и Ника"] = result
+
+    def test_andrey_and_nick(self):
+        turn_2 = Tournament(90, self.runners[1], self.runners[2])
+        result = turn_2.start()
+        self.assertTrue(list(result.values())[-1].name == "Ник")
+        self.all_results["Результат Андрея и Ника"] = result
+
+    def test_usain_andrey_nick(self):
+        turn_3 = Tournament(90, *self.runners)
+        result = turn_3.start()
+        self.assertTrue(list(result.values())[-1].name == "Ник")
+        self.all_results["Результат общего забега"] = result
+
+
+
+if __name__ == "__main__":
+    unittest.main()
